20e7f55523d92ba2ccc3cc8626d464223dada310
Eva Björling <eva.bjorling@chasacademy.se>
2024-05-06 12:45:56 +0200

    routing renewed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Layout from "./Layout/Layout";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import InfoPage from "./pages/InfoPage";
 import SchedulePage from "./pages/SchedulePage";
@@ -11,19 +11,17 @@ import Authentication from "./pages/Authenticaction.jsx";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/info" element={<InfoPage />} />
-            <Route path="/schedule" element={<SchedulePage />} />
-            <Route path="/guest-book" element={<GuestBookPage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/login" element={<Authentication />} />
-            <Route path="*" element={<MissingPage />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/info" element={<InfoPage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+          <Route path="/guest-book" element={<GuestBookPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/login" element={<Authentication />} />
+          <Route path="*" element={<MissingPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
